12b92d589d4ccdd22c84a4855dcf19e722f95d1f
Arnaud Soulier <soulier.arnaud@hotmail.fr>
2016-11-09 08:54:53 +0100

    Add Expression

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
+import model.Expression;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Goodbye World!");
+        Expression f1 = new Expression("p>((p>q)>q)", false, 0);
+        System.out.println(f1.toString());
+        Expression f2 = new Expression("p>(!q>!(p>q))", false, 0);
+        System.out.println(f2.toString());
+        Expression f3 = new Expression("(!p>!q)>(q>p)", false, 0);
+        System.out.println(f3.toString());
+        Expression f4 = new Expression("(p^(p>q)^((p>q)>r))>(p^q^r)", false, 0);
+        System.out.println(f4.toString());
     }
 }
--- src/model/Expression.java
+++ src/model/Expression.java
@@ -1,4 +1,96 @@
 package model;
 
 public class Expression {
+    private char name;
+    private Expression firstElement;
+    private Expression secondElement;
+    private char operator;
+    private boolean negative;
+
+    private static int stringProgression;
+
+    public Expression(char name, boolean negative) {
+        this.name = name;
+        this.firstElement = null;
+        this.secondElement = null;
+        this.negative = negative;
+    }
+
+    public Expression(String expression, boolean negative, int index) {
+        this.stringProgression = index;
+        StringBuffer buff = new StringBuffer(expression);
+        if(buff.charAt(this.stringProgression) == '!') {
+            this.stringProgression++;
+            if(buff.charAt(this.stringProgression) == '(') {
+                this.stringProgression++;
+                this.firstElement = new Expression(buff.toString(), true, this.stringProgression);
+            } else {
+                this.firstElement = new Expression(buff.charAt(this.stringProgression), true);
+                this.stringProgression++;
+            }
+        } else {
+            if(buff.charAt(this.stringProgression) == '(') {
+                this.stringProgression++;
+                this.firstElement = new Expression(buff.toString(), false, this.stringProgression);
+            } else {
+                this.firstElement = new Expression(buff.charAt(this.stringProgression), false);
+                this.stringProgression++;
+            }
+        }
+
+        this.operator = buff.charAt(this.stringProgression);
+        this.stringProgression++;
+        this.negative = negative;
+
+        if(buff.charAt(this.stringProgression) == '!') {
+            this.stringProgression++;
+            if(buff.charAt(this.stringProgression) == '(') {
+                this.stringProgression++;
+                this.secondElement = new Expression(buff.toString(), true, this.stringProgression);
+            } else {
+                this.secondElement = new Expression(buff.charAt(this.stringProgression), true);
+                this.stringProgression++;
+            }
+        } else {
+            if(buff.charAt(this.stringProgression) == '(') {
+                this.stringProgression++;
+                this.secondElement = new Expression(buff.toString(), false, this.stringProgression);
+            } else {
+                this.secondElement = new Expression(buff.charAt(this.stringProgression), false);
+                this.stringProgression++;
+                this.stringProgression++;
+            }
+        }
+    }
+
+    public String toString() {
+        if(this.firstElement == null) {
+            if(this.negative) {
+                return "!" + this.name;
+            } else {
+                return "" + this.name;
+            }
+        } else {
+            StringBuffer buff = new StringBuffer();
+            if(this.negative) {
+                buff.append("!");
+            }
+            buff.append("(");
+            buff.append(this.firstElement.toString());
+            switch(this.operator) {
+                case '>':
+                    buff.append(" -> ");
+                    break;
+                case '&':
+                    buff.append(" ^ ");
+                    break;
+                case '|':
+                    buff.append(" v ");
+                    break;
+            }
+            buff.append(this.secondElement.toString());
+            buff.append(")");
+            return buff.toString();
+        }
+    }
 }

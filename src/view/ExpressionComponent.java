990ef770e9508e5063419e979d066d316094af7f
Etienne Lefebvre <etiennelefebvre@outlook.com>
2016-11-16 11:23:31 +0100

    Added basic Expression Visualisation and Solving

--- src/view/ExpressionComponent.java
+++ src/view/ExpressionComponent.java
@@ -3,27 +3,100 @@ package view;
 import model.Expression;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 
 public class ExpressionComponent {
 
     private Expression expression;
-    private JPanel     panel;
+    private JPanel     panel, bottompanel;
     private JLabel     name;
+    private ArrayList<ExpressionComponent> subExpressionComponent;
 
     public ExpressionComponent (Expression e) {
         this.expression = e;
 
         this.panel = new JPanel();
-        this.name  = new JLabel(e.toString());
-        this.panel.add(this.name);
+        this.panel.setLayout(new BorderLayout());
+
+        this.name  = new JLabel(e.toString(), SwingConstants.CENTER);
+        this.name.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                solveExpression();
+            }
+        });
+
+        this.panel.add(this.name, BorderLayout.PAGE_START);
+
+        subExpressionComponent = new ArrayList<>();
     }
 
-    public ArrayList<ExpressionComponent> split () {
+    public void solveExpression() {
 
-        ArrayList<ExpressionComponent> subexpressions = new ArrayList<>();
-        return subexpressions;
+        if (this.expression.hasSubExpression()) {
 
+            Expression e = this.expression.solveExpression();
+            ArrayList<Expression> subExpression;
+
+            this.bottompanel = new JPanel();
+
+            switch (e.subExpressionNumber()) {
+
+                case 2:
+
+                    String expressiontype = e.getClass().getSimpleName();
+                    GridLayout layout = null;
+
+                    switch (expressiontype) {
+                        case "Conjonction":
+
+                            System.out.println("Conjonction");
+                            layout = new GridLayout(2, 1);
+
+                            break;
+
+                        case "Disjunction":
+                        case "Implication":
+
+                            System.out.println("Disjunction / Implication");
+                            layout = new GridLayout(1, 2);
+
+                            break;
+                    }
+
+                    subExpression = e.getSubExpressions();
+                    System.out.println(e);
+                    System.out.println(subExpression);
+                    this.subExpressionComponent.add(new ExpressionComponent(subExpression.get(0)));
+                    this.subExpressionComponent.add(new ExpressionComponent(subExpression.get(1)));
+
+
+                    this.bottompanel.setLayout(layout);
+
+                    this.bottompanel.add(this.subExpressionComponent.get(0).getPanel());
+                    this.bottompanel.add(this.subExpressionComponent.get(1).getPanel());
+
+                    break;
+
+                case 1:
+
+                    this.subExpressionComponent.add(new ExpressionComponent(e));
+
+                    this.bottompanel.add(this.subExpressionComponent.get(0).getPanel());
+
+                    break;
+            }
+
+            this.panel.add(this.bottompanel, BorderLayout.CENTER);
+
+            this.bottompanel.validate();
+            this.bottompanel.repaint();
+
+            this.panel.validate();
+            this.panel.repaint();
+        }
     }
 
     public JPanel getPanel() {
--- src/view/Window.java
+++ src/view/Window.java
@@ -1,4 +1,33 @@
 package view;
 
+import model.Expression;
+
+import javax.swing.*;
+import java.awt.*;
+
 public class Window {
+
+    private JFrame frame;
+    private ExpressionComponent firstexpression;
+
+    public Window (Expression e) {
+
+        this.frame = new JFrame();
+        this.frame.setTitle("The Logic Game");
+        this.frame.setLocation(50, 50);
+        this.frame.setSize(new Dimension(800, 600));
+        this.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception exc) {
+            System.err.println("Could not retrieve System look and feel");
+            exc.printStackTrace();
+        }
+
+        this.firstexpression = new ExpressionComponent(e);
+        this.frame.add(this.firstexpression.getPanel());
+
+        this.frame.setVisible(true);
+    }
 }
